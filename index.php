500377cc126100fe6c574d92cbcb8314b5214066
Michael Cavey <michaelcavey@michaels-imac.attlocal.net>
2016-04-27 14:15:31 -0700

    Add new index file

--- index.php
+++ index.php
@@ -4,16 +4,61 @@
     <meta charset="utf-8">
     <title>HOME Page | CS-313</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/cs313php.css" type="text/css" rel="stylesheet" media="screen">
 </head>
 <body>
     <header>
+        <img src="assets/wdd-logo.jpeg" alt="Graphic Logo"/>
         <div>
-        <h1>-HELLO WORLD-</h1>
+        <p>-IN LOVING MEMORY-</p>
+        <p>Remembering the Disappeared</p>
+        <p>Please Input your Information</p>
         </div>
     </header>
-        
+        <nav>
+            <ul>
+                <li><a href="index.php" title="Welcome | HOME Page">HOME</a></li>
+                <li><a href="assets/data-out.php" title="Database Outputs">Data</a></li>
+                <li><a href="assets/persons.php" title="Individual Profiles">Profiles</a></li>
+                <li><a href="assets/data-in.php" title="Database Inputs">FORM</a></li>
+            </ul>
+        </nav>  
     <main> 
-        <h3>From: Mike Cavey</h3>    
+        <h2>Introduction</h2>
+        <p>Welcome to the site where <strong><em>you</em></strong> can make a difference.</p>
+        <p>An estimated 100,000 people have disappeared in Latin American countries 
+           over the past sixty years.</p>
+        <p>Restore their memory and history here. Please record the life and times of your missing loved one.</p>
+        <p>We are planning the building of a permanent monument in memory of the disappeared.</p> 
+        <p>Please help use with a donation.</p>
+        
+        <h2>About Us</h2>
+        <p>We are a non-profit organization seeking the names of all those lost in Mexico, Central America, and South America.</p>
+        <p>We hope to build the most complete database of this kind, dedicated to preserving the memory of thousands.</p> 
+        <p>We hope to also build a memorial that would enshrine the names of all those lost.</p>
+        <p>Anyone can input information. Just click on the FORM link to build the database.</p>
+        
+        <h2>Contact Us</h2>
+        <p>Hello, my name is Michael Cavey.</p>
+        <img src="assets/my-photo.jpg" alt="Photograph of Organizer"/>
+        <p>If you have any questions, please contact us at <a href="index.php" title="EMAIL">"cav13002@byui.edu"</a>.</p>
+        <br>
     </main> 
+    <footer>
+        <nav>
+            <ul>
+                <li><a href="index.php" title="Welcome">HOME</a></li>
+                <li><a href="assets/site-plan.php" title="View the Site Plan">Site Plan</a></li>
+                <li><a href="assets/site-map.php" title="Site Map">Site Map</a></li>
+<!--                 <li><a href="assets/assignments.php" title="Course Work">Assignments</a></li>
+-->
+            </ul>
+        </nav>
+        <br>
+        <br>
+        <br>
+            <p><strong>Disclaimer:</strong> This web site is presented as course work for CS-313 at BYU-Idaho.</p>
+            <p>&#169; 2016 Michael Cavey. All rights reserved.</p>
+    </footer>
 </body>
 </html>
