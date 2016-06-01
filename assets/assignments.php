20bd62225fb13f3f8464f9bd62dd4b31fc7f0b28
Michael Cavey <michaelcavey@michaels-imac.attlocal.net>
2016-06-01 14:02:26 -0700

    Implement password

--- assets/assignments.php
+++ assets/assignments.php
@@ -29,6 +29,7 @@
             <li><a href="wk6-index.php" title="Week-6 Solo Attempt">Week-6 Solo Attempt</a></li>
             <li><a href="index.php" title="Week-6 Team Activity">Week-6 Team Activity</a></li>
             <li><a href="topicEntry.php" title="Week-6 Solution">Week-6 Solution</a></li>
+            <li><a href="account/index.php" title="Week-7 Solo Attempt">Week-7 Solo Attempt</a></li>
         </ul>
        <br>
     </main>
