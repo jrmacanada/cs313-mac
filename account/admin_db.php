406efd200e26f5e4a0021ecfc813769e1fc168ae
Michael Cavey <michaelcavey@michaels-imac.attlocal.net>
2016-06-01 16:48:52 -0700

    Implement password

--- account/admin_db.php
+++ account/admin_db.php
@@ -1,4 +1,39 @@
 <?php
+$db = dbConnect();
+
+function dbConnect() {
+
+	$dbHost = '';
+	$dbPort = '';
+	$dbUser = '';
+	$dbPassword = '';
+	$dbName = 'php';
+
+	$onOpenShift = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+	if ($onOpenShift === null || $onOpenShift== "") 
+	{
+		// in our localhost environment
+		$dbHost = '127.0.0.1';
+		$dbPort = '8889';
+		$dbUser = 'root';
+		$dbPassword = 'root';
+	}
+	else {
+		//in our OpenShift environment
+		$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+		$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+		$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	}
+
+//mac	echo "DB HOST:$dbHost:$dbPort dbName:$dbName user:$dbUser";
+
+	$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	return $db;
+}
+
 function is_valid_admin_login($email, $password) {
     global $db;
     $password = sha1($email . $password);
