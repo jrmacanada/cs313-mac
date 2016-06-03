51fd6290cd0c03e6d456c5f7a84e1e48e0b191ad
Michael Cavey <michaelcavey@michaels-imac.attlocal.net>
2016-06-03 16:24:55 -0700

    Login Page

--- login/alert2.php
+++ login/alert2.php
@@ -6,7 +6,7 @@
 
 <body>
 <div>
-    <p><a href="../index.php" title="Return to Sign-Up Page">Return to Sign-Up Page</a></p>
+    <p><a href="signUp.php" title="Return to Sign-Up Page">Return to Sign-Up Page</a></p>
     <script>
     window.alert("You must have a username & password");
     </script>
